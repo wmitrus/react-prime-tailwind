45d9402a3bc47c269ec312df39c205f8f8b415ed
Wojciech Mitrus <wmitrus@gmail.com>
2024-04-08 12:39:31 +0200

    feat(primereact): add react config and all needed imports with correct order

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
 import './index.css';
+import 'primeicons/primeicons.css';
+import { PrimeReactProvider } from 'primereact/api';
+import 'primeflex/primeflex.css';
+import 'primereact/resources/primereact.min.css';
+import 'primereact/resources/themes/lara-dark-cyan/theme.css';
+
+import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <PrimeReactProvider>
+      <App />
+    </PrimeReactProvider>
   </React.StrictMode>
 );
