7aacd694c4bae0342326d89000a98caa482dbb55
Wojciech Mitrus <wmitrus@gmail.com>
2024-04-08 21:35:57 +0200

    feat(themes): add LayoutContext, rearrange css imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// import './index.css';
 import 'primeicons/primeicons.css';
 import { PrimeReactProvider } from 'primereact/api';
 import 'primeflex/primeflex.css';
 import 'primereact/resources/primereact.min.css';
-import 'primereact/resources/themes/lara-dark-cyan/theme.css';
+// import 'primereact/resources/themes/lara-dark-cyan/theme.css';
 
+import { LayoutProvider } from './store/LayoutContext.tsx';
 import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <PrimeReactProvider>
-      <App />
+      <LayoutProvider>
+        <App />
+      </LayoutProvider>
     </PrimeReactProvider>
   </React.StrictMode>
 );
---
+++ types/index.d.ts
@@ -0,0 +1,52 @@
+import { ReactNode } from 'react';
+import {
+  Page,
+  AppBreadcrumbProps,
+  Breadcrumb,
+  BreadcrumbItem,
+  MenuProps,
+  MenuModel,
+  //   AppSubMenuProps,
+  LayoutConfig,
+  LayoutState,
+  //   AppBreadcrumbState,
+  Breadcrumb,
+  LayoutContextProps,
+  MailContextProps,
+  MenuContextProps,
+  ChatContextProps,
+  TaskContextProps,
+  AppConfigProps,
+  NodeRef,
+  AppTopbarRef,
+  //   MenuModelItem,
+  AppMenuItemProps,
+  AppMenuItem,
+} from './layout';
+
+type ChildContainerProps = {
+  children: ReactNode;
+};
+
+export type {
+  Page,
+  AppBreadcrumbProps,
+  Breadcrumb,
+  BreadcrumbItem,
+  MenuProps,
+  MenuModel,
+  LayoutConfig,
+  LayoutState,
+  Breadcrumb,
+  LayoutContextProps,
+  MailContextProps,
+  MenuContextProps,
+  ChatContextProps,
+  TaskContextProps,
+  AppConfigProps,
+  NodeRef,
+  AppTopbarRef,
+  AppMenuItemProps,
+  ChildContainerProps,
+  AppMenuItem,
+};
